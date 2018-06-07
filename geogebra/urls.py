7f5620da3d049239ff59a836165b97bbcc18dbca
Stasianna <nickah@inbox.ru>
2018-06-07 11:48:36 +0300

    dobavka

--- geogebra/urls.py
+++ geogebra/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.static import serve
 
+from geogebra_applet.views import MainPageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('geogebra-applet/',
          include('geogebra_applet.urls', namespace='geogebra_applet')),
+    path ('', MainPageView.as_view(), name='main_page'),
 ]
 
 if settings.DEBUG:
--- geogebra_applet/views.py
+++ geogebra_applet/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView, TemplateView
 
 from geogebra_applet.models import GeogebraApplet
 
 
 class GeogebraAppletDetailView(DetailView):
     model = GeogebraApplet
+
+class MainPageView(TemplateView):
+    template_name = 'geogebra_applet/main_str.html'
